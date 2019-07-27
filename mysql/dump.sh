e984b2499ae7b29c4ce9b62ea2dece434d7b4d85
haq0003 <32303755+haq0003@users.noreply.github.com>
2019-07-27 19:57:21 +0200

    Update dump.sh

--- mysql/dump.sh
+++ mysql/dump.sh
@@ -8,3 +8,7 @@ MYSQL_PASSWORD="XXXXXX"
 MYSQLDUMP=/usr/bin/mysqldump
 
 $MYSQLDUMP --single-transaction --add-drop-table --opt --user=$MYSQL_USER -p$MYSQL_PASSWORD XXXXXX  $db | gzip > "$BACKUP_DIR/${TIMESTAMP}_XXXXXX.gz"
+
+# Command Direct 
+# nice -19 mysqldump --add-drop-table --single-transaction  -u"XXXXXXXXXXXX" -p"XXXXXX" XXXXXX \ 
+# | mysql  --max_allowed_packet=500M -h"XXXXXX" -P"XXXXXX" -u"XXXXXX" -p"XXXXXX" 
